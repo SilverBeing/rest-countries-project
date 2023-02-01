569e1262f4f970703e22f786da9f18823ced3268
Silver Idahosa <108458852+SilverBeing@users.noreply.github.com>
2023-02-01 17:54:24 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+};
+export const eslint = {
+  // Warning: This allows production builds to successfully complete even if
+  // your project has ESLint errors.
   ignoreDuringBuilds: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
