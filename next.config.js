74a15539567184dd712064554a57ec54f2991ce4
Silver Idahosa <108458852+SilverBeing@users.noreply.github.com>
2023-02-03 15:25:39 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -2,10 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-export const eslint = {
-  // Warning: This allows production builds to successfully complete even if
-  // your project has ESLint errors.
-  ignoreDuringBuilds: true,
-};
 
 export default nextConfig;
